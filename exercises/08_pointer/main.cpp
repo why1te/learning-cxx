cee45a3957fe0130c370da9c29539c1720fad618
you <youngtingwang@gmail.com>
2026-08-03 12:44:30 +0800

    feat: Completed first 8 tests

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,14 +5,23 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+    int num = 0;
+    for(int *base = ptr; num < len -2; base += stride)
+    {
+        if(*(base + 2*stride) != *(base + stride) + *(base))
+        {
+            return false;
+        }
+        num++;
+    }
     return true;
 }
 
 // ---- 不要修改以下代码 ----
 int main(int argc, char **argv) {
-    int arr0[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55},
-        arr1[]{0, 1, 2, 3, 4, 5, 6},
-        arr2[]{99, 98, 4, 1, 7, 2, 11, 3, 18, 5, 29, 8, 47, 13, 76, 21, 123, 34, 199, 55, 322, 0, 0};
+    int arr0[11]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55},
+        arr1[7]{0, 1, 2, 3, 4, 5, 6},
+        arr2[23]{99, 98, 4, 1, 7, 2, 11, 3, 18, 5, 29, 8, 47, 13, 76, 21, 123, 34, 199, 55, 322, 0, 0};
     // clang-format off
     ASSERT( is_fibonacci(arr0    , sizeof(arr0) / sizeof(*arr0)    , 1),         "arr0 is Fibonacci"    );
     ASSERT( is_fibonacci(arr0 + 2, sizeof(arr0) / sizeof(*arr0) - 4, 1), "part of arr0 is Fibonacci"    );
